41915a16d298ae2b06ac75be39f953ab06530586
gitocean <1902366297@qq.com>
2016-10-11 10:47:48 +0800

    Initial commit

---
+++ app/src/main/java/com/gengchao/butterknife/MainActivity.java
@@ -0,0 +1,37 @@
+package com.gengchao.butterknife;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView();
+        ButterKnife.bind(this);
+    }
+
+
+
+    @OnClick(R.id.tv)
+    public void onClick() {
+
+    }
+
+    static class ViewHolder {
+        @Bind(R.id.tv)
+        TextView tv;
+
+        ViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
+    }
+}
